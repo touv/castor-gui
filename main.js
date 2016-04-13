4ebe28c4bdeb815b00e09a0a5a2e92d838cc9e2b
Nicolas Thouvenin <nthouvenin@gmail.com>
2016-04-13 22:43:15 +0200

    add infinty scroll

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import sharedStore from './store.js'
+import infiniteScroll from 'vue-infinite-scroll';
 
+Vue.use(infiniteScroll)
 Vue.use(VueResource)
 
 Vue.filter('uri', function (input) {
